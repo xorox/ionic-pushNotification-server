c74e407c12f9803874eba433e5b533d227b53c96
Robin <mail@xorox.ch>
2015-04-11 18:57:18 +0200

    added ios and android support of notification

--- routes/news.js
+++ routes/news.js
@@ -50,73 +50,71 @@ module.exports = function (app) {
 
     news.save(function (err, news) {
       process.nextTick(function () {
-        sendPushiOS(news);
-        sendPushAndroid(news)
+        User.find({deviceRegistered: true}).lean().exec(function (err, users) {
+          if (!err) {
+            for (var i = 0; i < users.length; i++) {
+              if(users[i].deviceToken.length < 70) {
+                sendPushiOS(users[i], news);
+              } else {
+                sendPushAndroid(users[i], news);
+              }
+            }
+          }
+        });
       });
       res.redirect('/add-news');
     });
   });
 
-  function sendPushiOS(news) {
+  function sendPushiOS(user, news) {
     var text = news.text.substr(0, 100);
-    User.find({deviceRegistered: true}).lean().exec(function (err, users) {
-      if (!err) {
-        for (var i = 0; i < users.length; i++) {
-          var user = users[i];
 
-          var device = new apn.Device(user.deviceToken);
-          var note = new apn.Notification();
-          note.badge = 1;
-          note.contentAvailable = 1;
-          note.alert = {
-            body : text
-          };
-          note.device = device;
+    var device = new apn.Device(user.deviceToken);
+    var note = new apn.Notification();
+    note.badge = 1;
+    note.contentAvailable = 1;
+    note.alert = {
+      body : text
+    };
+    note.device = device;
 
-          var options = {
-            gateway: 'gateway.sandbox.push.apple.com',
-            errorCallback: function(error){
-              console.log('push error', error);
-            },
-            cert: 'PushNewsCert.pem',
-            key:  'PushNewsKey.pem',
-            passphrase: 'superpass',
-            port: 2195,
-            enhanced: true,
-            cacheLength: 100
-          };
-          var apnsConnection = new apn.Connection(options);
-          console.log('push sent to ', user.username);
-          apnsConnection.sendNotification(note);
-        }
-      }
-    });
+    var options = {
+      gateway: 'gateway.sandbox.push.apple.com',
+      errorCallback: function(error){
+        console.log('push error', error);
+      },
+      cert: 'PushNewsCert.pem',
+      key:  'PushNewsKey.pem',
+      passphrase: 'superpass',
+      port: 2195,
+      enhanced: true,
+      cacheLength: 100
+    };
+    var apnsConnection = new apn.Connection(options);
+    console.log('push sent to ', user.username);
+    apnsConnection.sendNotification(note);
+
   }
 
-  function sendPushAndroid(news) {
+  function sendPushAndroid(user, news) {
     var gcmObject = new gcm.AndroidGcm('AIzaSyBXWKjEbzCfAersPzodMQrhDWguB5J5O7g');
 
     var registration_ids = Array();
-    User.find({deviceRegistered: true}).lean().exec(function (err, users) {
-      if (!err) {
-        for (var i = 0; i < users.length; i++) {
-          registration_ids.push(users[i].deviceToken);
+    registration_ids.push(users.deviceToken);
+
+    var message = new gcm.Message({
+        registration_ids: registration_ids,
+        data: {
+            key1: news
         }
+    });
 
-        var message = new gcm.Message({
-            registration_ids: registration_ids,
-            data: {
-                key1: news
-            }
-        });
+    gcmObject.send(message, function(err, response) {
+      if(err)
+        console.log(err+' '+response);
 
-        gcmObject.send(message, function(err, response) {
-          if(err)
-            console.log(err+' '+response);
-
-          console.log(response);
-        });
-      }
-    }
+      console.log(response);
+    });
+          
   }
 };
