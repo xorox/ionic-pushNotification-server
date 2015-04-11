e4e48ca612a562a277141e23d38c8d677ddc5ef8
Robin <mail@xorox.ch>
2015-04-11 19:03:26 +0200

    routs changed

--- app/routes/news.js
+++ app/routes/news.js
@@ -1,10 +1,15 @@
 // routes/news.js
 
 var mongoose = require('mongoose');
+//var User = mongoose.model('User');
+
+// iOS APN Client
 var News = mongoose.model('News');
-var Users = mongoose.model('User');
 var apn = require('apn');
 var _ = require('lodash');
+
+// Android GCM Client
+var gcm = require('android-gcm');
 
 module.exports = function (app) {
   'use strict';
@@ -44,51 +49,72 @@ module.exports = function (app) {
     });
 
     news.save(function (err, news) {
-      if(err)
-        return res.json({error: err});
-      else {
-        process.nextTick(function () {
-          sendPush(news);
+      process.nextTick(function () {
+        User.find({deviceRegistered: true}).lean().exec(function (err, users) {
+          if (!err) {
+            for (var i = 0; i < users.length; i++) {
+              if(users[i].deviceToken.length < 70) {
+                sendPushiOS(users[i], news);
+              } else {
+                sendPushAndroid(users[i], news);
+              }
+            }
+          }
         });
-      }
-
-      res.json({message: 'News '+text+' created!'});
+      });
+      res.redirect('/add-news');
     });
   });
 
-  function sendPush(news) {
+  function sendPushiOS(user, news) {
     var text = news.text.substr(0, 100);
-    Users.find({deviceRegistered: true}).lean().exec(function (err, users) {
-      if (!err) {
-        for (var i = 0; i < users.length; i++) {
-          var user = users[i];
 
-          var device = new apn.Device(user.deviceToken);
-          var note = new apn.Notification();
-          note.badge = 1;
-          note.contentAvailable = 1;
-          note.alert = {
-            body : text
-          };
-          note.device = device;
+    var device = new apn.Device(user.deviceToken);
+    var note = new apn.Notification();
+    note.badge = 1;
+    note.contentAvailable = 1;
+    note.alert = {
+      body : text
+    };
+    note.device = device;
 
-          var options = {
-            gateway: 'gateway.sandbox.push.apple.com',
-            errorCallback: function(error){
-              console.log('push error', error);
-            },
-            cert: 'PushNotificationsCert.pem',
-            key:  'CordovaPushNotificationDemoKey.pem',
-            passphrase: 'abc23',
-            port: 2195,
-            enhanced: true,
-            cacheLength: 100
-          };
-          var apnsConnection = new apn.Connection(options);
-          console.log('push sent to ', user.username);
-          apnsConnection.sendNotification(note);
+    var options = {
+      gateway: 'gateway.sandbox.push.apple.com',
+      errorCallback: function(error){
+        console.log('push error', error);
+      },
+      cert: 'PushNewsCert.pem',
+      key:  'PushNewsKey.pem',
+      passphrase: 'superpass',
+      port: 2195,
+      enhanced: true,
+      cacheLength: 100
+    };
+    var apnsConnection = new apn.Connection(options);
+    console.log('push sent to ', user.username);
+    apnsConnection.sendNotification(note);
+
+  }
+
+  function sendPushAndroid(user, news) {
+    var gcmObject = new gcm.AndroidGcm('AIzaSyBXWKjEbzCfAersPzodMQrhDWguB5J5O7g');
+
+    var registration_ids = Array();
+    registration_ids.push(users.deviceToken);
+
+    var message = new gcm.Message({
+        registration_ids: registration_ids,
+        data: {
+            key1: news
         }
-      }
     });
+
+    gcmObject.send(message, function(err, response) {
+      if(err)
+        console.log(err+' '+response);
+
+      console.log(response);
+    });
+
   }
 };
