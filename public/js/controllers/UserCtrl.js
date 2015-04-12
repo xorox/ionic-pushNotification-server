0fe52df96e33a7042af88b5b7fa2bd4b7341ef7c
Robin <mail@xorox.ch>
2015-04-12 16:57:10 +0200

    dfd

---
+++ public/js/controllers/UserCtrl.js
@@ -0,0 +1,6 @@
+// public/js/controllers/NerdCtrl.js
+angular.module('UserCtrl', []).controller('UserController', function($scope) {
+
+    $scope.tagline = 'Nothing beats a pocket protector!';
+
+});
